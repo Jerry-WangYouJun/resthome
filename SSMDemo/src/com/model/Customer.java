f35c5ff915878df86e54faada8f74a2fb2a3f76d
Jerry_Wang <Administrator@PC-201604301005>
2017-04-24 17:32:39 +0800

    新增老人信息查询

---
+++ SSMDemo/src/com/model/Customer.java
@@ -0,0 +1,111 @@
+package com.model;
+
+public class Customer {
+	  
+	 Integer id ; 
+	 Integer bedId ; 
+	 String cname ; 
+	 String sex ;
+	 String birthday ; 
+	 String level ;
+	 String idcard ;
+	 String address;
+	 String nurse;
+	 String indate ; 
+	 String outdate;
+	 String relation;
+	 String rphone;
+	 String rname;
+	 String raddress;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getBedId() {
+		return bedId;
+	}
+	public void setBedId(Integer bedId) {
+		this.bedId = bedId;
+	}
+	public String getCname() {
+		return cname;
+	}
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+	public String getLevel() {
+		return level;
+	}
+	public void setLevel(String level) {
+		this.level = level;
+	}
+	public String getIdcard() {
+		return idcard;
+	}
+	public void setIdcard(String idcard) {
+		this.idcard = idcard;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getNurse() {
+		return nurse;
+	}
+	public void setNurse(String nurse) {
+		this.nurse = nurse;
+	}
+	public String getIndate() {
+		return indate;
+	}
+	public void setIndate(String indate) {
+		this.indate = indate;
+	}
+	public String getOutdate() {
+		return outdate;
+	}
+	public void setOutdate(String outdate) {
+		this.outdate = outdate;
+	}
+	public String getRelation() {
+		return relation;
+	}
+	public void setRelation(String relation) {
+		this.relation = relation;
+	}
+	public String getRphone() {
+		return rphone;
+	}
+	public void setRphone(String rphone) {
+		this.rphone = rphone;
+	}
+	public String getRname() {
+		return rname;
+	}
+	public void setRname(String rname) {
+		this.rname = rname;
+	}
+	public String getRaddress() {
+		return raddress;
+	}
+	public void setRaddress(String raddress) {
+		this.raddress = raddress;
+	}
+	 
+}
---
+++ SSMDemo/src/com/service/CustomerService.java
@@ -0,0 +1,9 @@
+package com.service;
+
+import java.util.List;
+
+import com.model.Customer;
+
+public interface CustomerService {
+    List<Customer> findAllUser();
+}
