b12f7bc4af7df5431b658ab92f9c209de6a8164d
wang_yjun <wang_yjun@wang_yjun-PC>
2017-04-26 07:07:34 +0800

    更新数据库

--- SSMDemo/src/config/sql/resthome.sql
+++ SSMDemo/src/config/sql/resthome.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50130
 File Encoding         : 65001
 
-Date: 2017-04-25 22:48:37
+Date: 2017-04-26 07:07:14
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -43,13 +43,15 @@ CREATE TABLE `t_bed` (
   `area` varchar(20) DEFAULT NULL,
   `description` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of t_bed
 -- ----------------------------
 INSERT INTO `t_bed` VALUES ('1', 'B001', 'R001', 'A区', '1111\r\n              ');
+INSERT INTO `t_bed` VALUES ('12', '1111', '1', '1', ' \r\n                          1111\r\n                          \r\n                         \r\n          ');
 INSERT INTO `t_bed` VALUES ('6', 'T002', 'R002', 'A区', null);
+INSERT INTO `t_bed` VALUES ('9', '1', '1', '11', ' 1111\r\n                         \r\n              ');
 
 -- ----------------------------
 -- Table structure for `t_customer`
@@ -77,3 +79,44 @@ CREATE TABLE `t_customer` (
 -- ----------------------------
 -- Records of t_customer
 -- ----------------------------
+
+-- ----------------------------
+-- Table structure for `t_move`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_move`;
+CREATE TABLE `t_move` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `cname` varchar(20) DEFAULT NULL,
+  `oldroom` varchar(10) DEFAULT NULL,
+  `newroom` varchar(10) DEFAULT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_move
+-- ----------------------------
+INSERT INTO `t_move` VALUES ('1', '1', null, '1', null);
+
+-- ----------------------------
+-- Table structure for `t_room`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_room`;
+CREATE TABLE `t_room` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `roomcode` varchar(20) DEFAULT NULL,
+  `area` varchar(20) DEFAULT NULL,
+  `bednum` int(10) DEFAULT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_room
+-- ----------------------------
+INSERT INTO `t_room` VALUES ('10', '1', '11', '1', null);
+INSERT INTO `t_room` VALUES ('9', '1', '1', '1', null);
+INSERT INTO `t_room` VALUES ('5', '123', '123', '123', null);
+INSERT INTO `t_room` VALUES ('6', '111', '1111', '111', null);
+INSERT INTO `t_room` VALUES ('7', '123', '123123', '123', null);
+INSERT INTO `t_room` VALUES ('8', '11', '11', '11', null);
